d1703a0bc874741f1e220ced5a78daed1d28547b
zahrahabibimini <139326429+zahrahabibimini@users.noreply.github.com>
2023-08-24 20:38:50 +0430

    create templates

---
+++ root/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns =[
+    path('',home),
+    path('about',about),
+    path('contact',contact),
+]
--- root/views.py
+++ root/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request,'root/index.html')
+
+def about(request):
+    return render(request,'root/about.html')
+
+def contact(request):
+    return render(request,'root/contact.html')
+
+
+
