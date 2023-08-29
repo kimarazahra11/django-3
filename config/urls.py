9673287b4de2c869adf7b24f9a0578837f6776cc
zahra habibi <142114230+kimarazahra11@users.noreply.github.com>
2023-08-29 15:53:50 +0430

    new templates

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('root.urls')),
+    path('gallery/',include('gallery.urls')),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
 
+
+app_name = 'root'
 urlpatterns =[
-    path('',home),
-    path('about',about),
-    path('contact',contact),
+    path('',home,name='home'),
+    path('about',about,name='about'),
+    path('contact',contact,name='contact'),
 ]
